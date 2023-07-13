323d4924ec11ecfc09e378b6631c03454d39c7bd
yassirak <yasser.karimi.safi@gmail.com>
2023-07-13 11:08:40 -0700

    comm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 
 #include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * return: 0 (success)
+ */
 int main(void)
 {
 puts(“\”Programming is like building a multilingual puzzle”);
