6d06bf5379c9ecbfdb369307aa996e18e8c1ec48
yassirak <yasser.karimi.safi@gmail.com>
2023-07-18 10:23:22 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,15 @@
  * main - Entry point
  * Description :prints using putshar prototype
  * 
- * Return always 0
+ * Return always (success) 0
  */
 
 int main(void)
 {
-	char str[] = "putshar";
+	char str[] = "putchar";
 	int ch;
 	for (ch=0;ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
-	return  (0);
+	return(0);
 }
