38f52dfeabfd13b8f63f0877e0b3eb3945f8548c
yassirak <yasser.karimi.safi@gmail.com>
2023-07-18 11:43:43 -0700

    com

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-	char str[] = "putchar";
+	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
-		_putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
