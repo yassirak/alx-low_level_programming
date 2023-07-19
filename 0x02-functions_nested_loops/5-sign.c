1f382ce6ccae6f0b4bcbf6f4bd4d6837f550c7ef
yassirak <yasser.karimi.safi@gmail.com>
2023-07-19 12:34:51 -0700

    5 com

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,20 +11,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
+	_putchar(43);
+	return (1);
 	}
-	 else if (n == 0)
-	 {
-
-		_putchar(48);
-		return (0);
-	}
-       	else
+	else if (n == 0)
 	{
-		_putchar(45);
+	_putchar(48);
+	return (0);
+	}
+	else
+	{
+	_putchar(45);
 
-		return (-1);
+	return (-1);
 	}
 }
 
