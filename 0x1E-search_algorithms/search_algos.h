e369f3c1da9e6840427074a57e66d77715d3719a
yassirak <yasser.karimi.safi@gmail.com>
2024-03-13 09:04:34 -0700

    c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,12 @@
-#ifndef _search_algos_
-#define _search_algos_
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r);
-int binary_search_index(int *array, size_t l, size_t r, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
-#endif /* _search_algos_ */
+#endif /* SEARCH_ALGOS_H */
